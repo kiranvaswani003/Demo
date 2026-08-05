4d53579cd01af89e2cf7a0009adf26f735eaa0f3
kiran <kiranvaswani003@gmail.com>
2026-08-05 14:34:46 +0530

    fix: solutions to platform

--- program.java
+++ program.java
@@ -1,5 +1,6 @@
 class Program{
     public static void main(String ar[]){
-        System.out.println("Welcome to AveryCare Solutions");
+      //  System.out.println("Welcome to AveryCare Solutions");
+      System.out.println(" Welcome to AveryCare Platform");
     }
 }
